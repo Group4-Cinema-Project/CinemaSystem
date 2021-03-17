1e0c2bc18af95a168f15f0fa6a622cb06707b3d2
Reajel Cicilia <1018371@hr.nl>
2021-03-17 16:26:17 +0100

    GHELLO

--- Kaleidoscope app/Program.cs
+++ Kaleidoscope app/Program.cs
@@ -7,6 +7,7 @@ namespace Kaleidoscope_app
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("HEY PRACHTIG");
         }
     }
 }
