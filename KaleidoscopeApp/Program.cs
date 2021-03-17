e569c7e2b9f4dc72d0a12561b18cf824944f7b94
guido <guido@DESKTOP-F3UV7F8>
2021-03-17 16:19:41 +0100

    Sup bruh

--- KaleidoscopeApp/Program.cs
+++ KaleidoscopeApp/Program.cs
@@ -7,6 +7,7 @@ namespace KaleidoscopeApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Sup bruh");
         }
     }
 }
